54feb5fbc456985adccb9556dc98e26090b5abbe
josricor <josricor@outlook.com>
2021-09-16 13:24:48 +0800

    fix

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import os
-mobile = os.getenv('MOBILE', default = None)
-password = os.getenv('PASSWORD', default = None)
+mobile = os.getenv('YIBAN_MOBILE', default = None)
+password = os.getenv('YIBAN_PASSWORD', default = None)
 print(mobile)
 print(password)
 
+if "YIBAN_MOBILE" in os.environ:
+    print("已获取并使用Env环境 MOBILE")
+    
 if mobile is None:
   print('Error')
