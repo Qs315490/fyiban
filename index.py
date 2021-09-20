46015b09e45f12285e598f260169e9e666bf5de4
josricor <josricor@outlook.com>
2021-09-20 14:33:06 +0800

    DDrobot

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 import os
 import json
 import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
 import requests
 from yiban import Yiban
 
@@ -13,6 +17,8 @@ ENV = {
   'YIBAN_PASSWORD': 'YIBAN_PASSWORD',  # 密码
   'YIBAN_DATA': 'YIBAN_DATA',  # 提交表单1
   'YIBAN_EXTEND': 'YIBAN_DEXTEND',  # 提交表单2
+  'DD_BOT_TOKEN': 'DD_BOT_TOKEN',  # 钉钉推送
+  'DD_BOT_SECRET': 'DD_BOT_SECRET',  # 钉钉推送密钥
   'BARK_PUSH': 'BARK_PUSH'  # Bark
 }
 
@@ -27,7 +33,6 @@ def main():
   mobile = ''
   password = ''
   msg = ''
-  title = time.strftime("%m-%d", time.localtime()) + '易班打卡:\n'
 
   try:
     with open('./config.json', 'rb') as f:
@@ -73,17 +78,45 @@ def main():
     extend = json.loads(os.getenv('YIBAN_EXTEND'))
     print("已从环境变量中获取extend")
 
-
   result = name + Yiban(mobile, password, data, extend).main()
   msg = msg + '\n' + result
-  return title + msg
+  return msg
+
+
+def dingPush(token, secret, title, msg):
+  timestamp = str(round(time.time() * 1000))
+  secret_enc = secret.encode('utf-8')
+  string_to_sign = f'{timestamp}\n{secret}'
+  string_to_sign_enc = string_to_sign.encode('utf-8')
+  hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+  sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+  url = f'https://oapi.dingtalk.com/robot/send?access_token={token}&timestamp={timestamp}&sign={sign}'
+  data = {
+    "msgtype": "markdown",
+    "markdown": {
+        "title": title,
+        "text": f'### {time.strftime("%m-%d", time.localtime())} 易班打卡 \n {msg}'
+    }
+  }
+  requests.post(url, data=json.dumps(data), headers={
+    "Content-Type": "application/json",
+    "Charset": "UTF-8"
+  })
 
 
 if __name__ == '__main__':
-  result = main()
-  print(result)
+  msg = main()
+  title = f'{time.strftime("%m-%d", time.localtime())}易班打卡'
+  print(msg)
   if ENV['BARK_PUSH'] in os.environ:
     bark = os.getenv('BARK_PUSH')
     print("已从环境变量中获取bark")
-    requests.post(url=f'https://api.day.app/{bark}/{result}')
-    print("推送成功")
+    requests.post(url=f'https://api.day.app/{bark}/{title + msg}')
+    print("bark推送成功")
+  
+  if ENV['DD_BOT_TOKEN'] in os.environ:
+    if ENV['DD_BOT_SECRET'] in os.environ:
+      token = os.getenv('DD_BOT_TOKEN')
+      secret = os.getenv('DD_BOT_SECRET')
+      dingPush(token, secret, title, msg)
+      print("钉钉推送成功")
