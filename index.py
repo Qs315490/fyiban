8343793008a75fb57ef39b9a2798f08f2e191fe6
josricor <josricor@outlook.com>
2021-10-21 13:31:41 +0800

    update

--- index.py
+++ index.py
@@ -5,9 +5,10 @@
 # 易班校本化打卡
 
 import time
+import utils
 import random
 from yiban import Yiban
-from utils import wechat
+
 
 # 打卡列表
 # mobile: 账号
@@ -18,13 +19,9 @@ UserList = [
         {...}, 
     ]
 
-# 微信推送
-#agentid = ''
-#corpid = ''
-#corpsecret = ''
-
 # 提交表单
-# 需要手动抓包填写
+# 需要手动抓包
+# crypter.py aes_decrypt()解密填写
 PostData = {
     "WFId": "",
     "Data": {
@@ -32,13 +29,14 @@ PostData = {
         "72fe9c2c81a48fd0968edbbb3f2c1c42": f'36.{random.randint(3,8)}', 
         "769bbfbfb026629f1ddb0294a9c0d257": "示例位置", 
         "90b5b83950ae456fefebb0f751406e2d": [{ 
-            "name": "348816E7-275F-42D8-9118-625FB45D8D48.png ", 
+            "name": "348816E7-275F-42D8-9118-625FB45D8D48.png", 
             "type": "image/png ", 
             "size ": 472092, 
-            "status": "done ", "percent ": 100, 
+            "status": "done ", 
+            "percent ": 100, 
             "fileName": "workflow/202110/18/4c3599675b629e14e80ffe19c507b806.png ", 
             "path": "workflow/202110/18/4c3599675b629e14e80ffe19c507b806.png "
-            }],
+        }],
         "0242914b8746275c5073ccdb156f9d1d": [{ 
             "name": "86E7DCB9-7FEE-441F-B921-039302A1048B.png", 
             "type": "image/png ", 
@@ -47,7 +45,7 @@ PostData = {
             "percent": 100, 
             "fileName": "workflow/202110/18/b82da80c3a4dc4ad6f4a1ebe325fb033.png ", 
             "path": "workflow/202110/18/b82da80c3a4dc4ad6f4a1ebe325fb033.png "
-            }]
+        }]
     },
     "Extend": { 
         "TaskId": "", 
@@ -67,8 +65,8 @@ def main():
             result = Yiban(i['mobile'], i['password']).submit(PostData)
             msg = f'{msg}\n {result["name"]}: {result["msg"]}'
             print(result)
-        except:
-            msg = f'{msg}\n {i["name"]}: Error'
+        except Exception as e:
+            msg = f'{msg}\n {i["name"]}: Error {e}'
             continue
         finally:
             time.sleep(1)
@@ -77,4 +75,12 @@ def main():
 if __name__ == '__main__':
     msg = main()
     print(msg)
-    #wechat(corpid, agentid, corpsecret, msg) # 企业微信推送
+    # 企业微信推送
+    # agentid = ''
+    # corpid = ''
+    # corpsecret = ''
+    # utils.wechat(corpid, agentid, corpsecret, msg) # 企业微信推送
+
+    # bark推送
+    # key = ''
+    # utils.bark(key, msg) # bark推送
--- utils.py
+++ utils.py
@@ -7,26 +7,42 @@
 import json
 import requests
 
-def wechat(corpid, agentid, corpsecret, msg):
+def wechat(corpid, agentid, corpsecret, msg, msgtype='text'):
     """
     企业微信推送
     corpid: 企业ID
     agentid: 应用ID
     corpsecret: 应用密钥
+    msg: 推送消息
+    msgtype: 消息类型 默认为text
     """
-    data = {
-        "touser" : "@all",
-        "msgtype" : "text",
-        "agentid" : agentid,
-        "text" : {"content" : msg},
-    }
-    access_token = requests.get(
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken', 
-        params = {'corpid': corpid, 'corpsecret': corpsecret}
-    ).json()['access_token']
+    try:
+        data = {
+            "touser" : "@all",
+            "msgtype" : msgtype,
+            "agentid" : agentid,
+            "text" : {"content" : msg},
+        }
+        access_token = requests.get(
+            url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken', 
+            params = {'corpid': corpid, 'corpsecret': corpsecret}
+        ).json()['access_token']
 
-    requests.post(
-        url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send', 
-        params = {'access_token': access_token}, 
-        data = json.dumps(data)
-    )
+        requests.post(
+            url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send', 
+            params = {'access_token': access_token}, 
+            data = json.dumps(data)
+        )
+    except Exception as e:
+        print(f'Error! Push Wechat {e}')
+
+def bark(key, msg):
+    """
+    bark推送
+    key: 推送密钥
+    msg: 推送消息
+    """
+    try:
+        requests.get(url=f'https://api.day.app/{key}/{msg}',)
+    except Exception as e:
+        print(f'Error! Push Bark {e}')
