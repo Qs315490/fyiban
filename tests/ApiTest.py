44f77f98d94db5deaea900757067efecde108889
Qs315490 <qs315490@qq.com>
2023-10-22 22:46:48 +0800

    update

--- tests/ApiTest.py
+++ tests/ApiTest.py
@@ -6,4 +6,6 @@ from yiban import Yiban
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-yb = Yiban("", "")
+yb = Yiban("test", "password")
+logging.info(yb.task_feedback.get_sign_task())
+logging.info(yb.submit_task_feedback())
--- yiban/Core/Login.py
+++ yiban/Core/Login.py
@@ -59,18 +59,10 @@ class Login:
         :return: Any
         """
         rsa_key = """-----BEGIN PUBLIC KEY-----
-            MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA6aTDM8BhCS8O0wlx2KzA
-            Ajffez4G4A/QSnn1ZDuvLRbKBHm0vVBtBhD03QUnnHXvqigsOOwr4onUeNljegIC
-            XC9h5exLFidQVB58MBjItMA81YVlZKBY9zth1neHeRTWlFTCx+WasvbS0HuYpF8+
-            KPl7LJPjtI4XAAOLBntQGnPwCX2Ff/LgwqkZbOrHHkN444iLmViCXxNUDUMUR9bP
-            A9/I5kwfyZ/mM5m8+IPhSXZ0f2uw1WLov1P4aeKkaaKCf5eL3n7/2vgq7kw2qSmR
-            AGBZzW45PsjOEvygXFOy2n7AXL9nHogDiMdbe4aY2VT70sl0ccc4uvVOvVBMinOp
-            d2rEpX0/8YE0dRXxukrM7i+r6lWy1lSKbP+0tQxQHNa/Cjg5W3uU+W9YmNUFc1w/
-            7QT4SZrnRBEo++Xf9D3YNaOCFZXhy63IpY4eTQCJFQcXdnRbTXEdC3CtWNd7SV/h
-            mfJYekb3GEV+10xLOvpe/+tCTeCDpFDJP6UuzLXBBADL2oV3D56hYlOlscjBokNU
-            AYYlWgfwA91NjDsWW9mwapm/eLs4FNyH0JcMFTWH9dnl8B7PCUra/Lg/IVv6HkFE
-            uCL7hVXGMbw2BZuCIC2VG1ZQ6QD64X8g5zL+HDsusQDbEJV2ZtojalTIjpxMksbR
-            ZRsH+P3+NNOZOEwUdjJUAx8CAwEAAQ==
+            MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnslKgSPbbGTy1p5pAknnVT2sr
+            58YNWGrHR/0TOF9HneSyQDubc7QaXwmbHlRtYqr/jcaQ+0PDRHcVdaHCLEcEm7U3
+            RaiZNP38vgYMTlKak5eywfcXBkvGhlGHJ6QVEGsPDsxKqCR8DX3malgfOw0wnkBV
+            dA5Xc8xsf+cydAbWDQIDAQAB
             -----END PUBLIC KEY-----
             """
         data = bytes(data, encoding="utf8")
