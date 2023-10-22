d83bfc86fcc5564b1674c6512ace4f3ad94a3e19
Qs315490 <qs315490@qq.com>
2023-10-22 23:04:05 +0800

    包名更改

--- yiban/__init__.py
+++ yiban/__init__.py
@@ -1,7 +1,7 @@
 """yiban"""
 from .Apis import *
 
-__title__ = 'yiban'
+__title__ = 'fyiban'
 __description__ = 'Apis lib for yiban.'
 __url__ = 'https://github.com/Qs315490/fyiban'
 __version__ = '1.0.8'
